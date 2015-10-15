be8afb4b1e8316798617e66808283910cc805311
tiagomartinho <tiago.martinho@xpeppers.com>
2015-10-15 20:21:57 +0200

    add UILabel to both storyboard and set the text in common view controller

--- tvOS/ViewController.swift
+++ tvOS/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var helloLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        helloLabel.text = "Hello World!"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
